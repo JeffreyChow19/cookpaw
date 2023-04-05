4bf178958623d6e31e1974c1d619632321a99b33
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-04-05 15:04:57 +0700

    fix: remove import from test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,11 @@
 # Change path directory to root
-import sys
-import os
-root_dir = os.path.abspath(os.path.join(os.getcwd()))
-sys.path.append(root_dir)
+# import sys
+# import os
+# root_dir = os.path.abspath(os.path.join(os.getcwd()))
+# sys.path.append(root_dir)
 
 # Import function cookPaw from main
-from src.main import *
+# from src.main import *
 
 # Test cookPaw function
 def test_main():
