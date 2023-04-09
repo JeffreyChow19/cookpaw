3a25ce8bc04268feb62a0d6487c55cbe7b289fc0
chaerla <chelgchen@gmail.com>
2023-04-09 23:05:21 +0800

    integrated controller with main

--- src/main.py
+++ src/main.py
@@ -4,25 +4,17 @@ import os.path
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
 from PyQt5.QtCore import pyqtSlot, QFile, QTextStream
 from ui.ui_main import Ui_MainWindow
-from database.db import *
+from controller.controller import *
 from models.article import *
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        if not os.path.exists("src/database/cookpaw.db"):
-            database = Database()
-        # create a connection to the database
-        conn = sqlite3.connect("src/database/cookpaw.db")
-        c = conn.cursor()
-        # execute a SELECT statement to retrieve data from the articles table
-        c.execute("SELECT * FROM articles")
-
-        # fetch all the rows from the result set
-        article_rows = c.fetchall()
+        controller = Controller("src/database/cookpaw.db")
+        articles_row = controller.get_all_articles()
 
         # create a list of Article objects from the rows
-        articles = [Article.from_row(row) for row in article_rows]
+        articles = [Article.from_row(row) for row in articles_row]
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self, articles)
