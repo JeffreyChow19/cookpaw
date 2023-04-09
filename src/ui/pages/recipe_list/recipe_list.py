26dd0fe83317281ae6b63a1b6d4502bc539957d1
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-04-09 16:52:10 +0700

    feat: added carousel of recipes and temp list_of_recipes

--- src/ui/pages/recipe_list/recipe_list.py
+++ src/ui/pages/recipe_list/recipe_list.py
@@ -2,6 +2,7 @@ from ui.components.card.recipe_card import *
 from ui.components.card.article_card import *
 from ui.components.searchbar.searchbar import *
 from ui.components.collectionButton.collectionButton import *
+from ui.components.cardsCarousel.cardsCarousel import *
 from ui.utils import getFont
 from PyQt5 import QtCore, QtGui, QtWidgets, QtSvg
 
@@ -14,9 +15,9 @@ class RecipeList(QtWidgets.QWidget):
         parentHeight = parent.height()
 
         #set dashboard size
-        self.setFixedWidth(int(0.95 * parentWidth))
+        self.setFixedWidth(int(0.9 * parentWidth))
         self.setFixedHeight(parentHeight)
-
+        
         ## HEADER ##
         # RECIPE LIST TITLE
         recipe_list_title = QtWidgets.QLabel()
@@ -26,19 +27,93 @@ class RecipeList(QtWidgets.QWidget):
         recipe_list_title.setObjectName("recipe_list_title")
         recipe_list_title.setStyleSheet("#recipe_list_title{color: #F15D36;}")
 
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addStretch()
+        self.layout.addWidget(recipe_list_title)
+
         # RECIPE COLLECTIONS AND SEARCH BAR
         cookpaw_collection_button = CollectionButton("Cookpaw's Collection", self)
         user_collection_button = CollectionButton("User's Collection", self)
 
         search_bar = SearchBar("Search Recipe Title", parent)
 
-        self.layout = QtWidgets.QVBoxLayout()
-        self.layout.addWidget(recipe_list_title)
-        self.layout.addWidget(cookpaw_collection_button)
-        self.layout.addWidget(user_collection_button)
-        self.layout.addWidget(search_bar)
+        collection_search_container = QtWidgets.QHBoxLayout()
+        collection_search_container.addWidget(cookpaw_collection_button)
+        collection_search_container.addWidget(user_collection_button)
+        collection_search_container.addStretch()
+        collection_search_container.addWidget(search_bar)
+
+        self.layout.addLayout(collection_search_container)
+
+        # RECIPE CARDS
+        list_recipes = [
+            {
+                "label" : "Crispy Pork Belly",
+                "file" : "image_pork_belly"
+            },
+            {
+                "label" : "Salt and Pepper Shrimp",
+                "file" : "image_salt_and_pepper_shrimp"
+            },
+            {
+                "label" : "Crispy Pork Belly",
+                "file" : "image_pork_belly"
+            },
+            {
+                "label" : "Salt and Pepper Shrimp",
+                "file" : "image_salt_and_pepper_shrimp"
+            },
+            {
+                "label" : "Crispy Pork Belly",
+                "file" : "image_pork_belly"
+            },
+            {
+                "label" : "Salt and Pepper Shrimp",
+                "file" : "image_salt_and_pepper_shrimp"
+            },
+            {
+                "label" : "Crispy Pork Belly",
+                "file" : "image_pork_belly"
+            },
+            {
+                "label" : "Salt and Pepper Shrimp",
+                "file" : "image_salt_and_pepper_shrimp"
+            },
+            {
+                "label" : "Crispy Pork Belly",
+                "file" : "image_pork_belly"
+            },
+            {
+                "label" : "Salt and Pepper Shrimp",
+                "file" : "image_salt_and_pepper_shrimp"
+            },
+        ]
+    
+        recipe_carousel = CardsCarousel('recipe', list_recipes, 6, self)
+        self.layout.addWidget(recipe_carousel)
+        self.layout.addStretch()
+
+        # ADD RECIPE BUTTON
+        add_button = QtWidgets.QToolButton(self)
+        add_button.setIcon(QtGui.QIcon("assets/icons/icon_add.svg"))
+        add_button.setFixedWidth(int(0.085 *self.height()))
+        add_button.setFixedHeight(int(0.085 *self.height()))
+        add_button.setObjectName("add_button")
+        add_button.setStyleSheet("""
+            #add_button { 
+                background-color: none; 
+                border: none; 
+            } 
+        """)
+        add_button.setIconSize(add_button.size())
+        add_button.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        
+        # MOVE TO THE BOTTOM RIGHT
+        add_button.move(self.width() - add_button.width(), int(0.8 * self.height()))
+
+        # Raise the button to the top of the parent widget's stack
+        add_button.raise_()
 
         self.setLayout(self.layout)
-    
 
    
