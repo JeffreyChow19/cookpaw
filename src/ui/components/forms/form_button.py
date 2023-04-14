33dce1838eb258ab0a18dddfa20981e5b4f32110
Alexander Jason <alexanderjason526@gmail.com>
2023-04-14 22:59:57 +0700

    feat: add button type

--- src/ui/components/forms/form_button.py
+++ src/ui/components/forms/form_button.py
@@ -2,7 +2,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets, QtSvg
 from ui.utils import *
 
 class FormButton(QtWidgets.QWidget):
-    def __init__(self, title, width_multiplier, parent=None):
+    def __init__(self, title, button_type, parent=None):
         super().__init__(parent)
 
         # SET WIDTH & REQUIRED
@@ -10,24 +10,46 @@ class FormButton(QtWidgets.QWidget):
 
         self.button_container = QtWidgets.QHBoxLayout()
 
-        # RECIPE COLLECTIONS AND SEARCH BAR
-        form_button = QtWidgets.QPushButton()
-        form_button.setFont(getFont("Bold", 11))
-        form_button.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-        form_button.setAutoFillBackground(False)
-        form_button.setText(title)
-        form_button.setObjectName( "form_button")
-        form_button.setStyleSheet( """
-            #form_button { 
-                text-align: center; 
-                color: black;
-                border: none;
-                border-radius: 15px;
-                background-color: #FFCF52;
-            } 
-        """)
-        form_button.setFixedHeight(int(0.06 * parent.height()))
-        form_button.setFixedWidth(int(width_multiplier * parent.width()))
+        if(button_type == "submit"):
+            # RECIPE COLLECTIONS AND SEARCH BAR
+            form_button = QtWidgets.QPushButton()
+            form_button.setFont(getFont("Bold", 11))
+            form_button.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+            form_button.setAutoFillBackground(False)
+            form_button.setText(title)
+            form_button.setObjectName("form_button")
+            form_button.setStyleSheet( """
+                #form_button { 
+                    text-align: center; 
+                    color: black;
+                    border: none;
+                    border-radius: 15px;
+                    background-color: #FFCF52;
+                } 
+            """)
+            form_button.setFixedHeight(int(0.06 * parent.height()))
+            form_button.setFixedWidth(int(0.4 * parent.width()))
+        else:
+            # RECIPE COLLECTIONS AND SEARCH BAR
+            form_button = QtWidgets.QPushButton()
+            attach_logo_path = "assets/icons/icon_attach.svg"
+            form_button.setIcon(QtGui.QIcon(attach_logo_path))
+            form_button.setFont(getFont("Bold", 11))
+            form_button.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+            form_button.setAutoFillBackground(False)
+            form_button.setText(title)
+            form_button.setObjectName("form_button")
+            form_button.setStyleSheet( """
+                #form_button { 
+                    text-align: center; 
+                    color: black;
+                    border: none;
+                    border-radius: 15px;
+                    background-color: #FFCF52;
+                } 
+            """)
+            form_button.setFixedHeight(int(0.06 * parent.height()))
+            form_button.setFixedWidth(int(0.2 * parent.width()))
 
         self.button_container.addStretch()
         self.button_container.addWidget(form_button)
