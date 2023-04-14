e20faa306d7fbb0d034f0692d97fce90cd05195b
Angela Livia Arumsari <16521177@mahasiswa.itb.ac.id>
2023-04-14 20:50:41 +0700

    fix: article content formatting

--- src/ui/pages/article_detail.py
+++ src/ui/pages/article_detail.py
@@ -1,4 +1,5 @@
 from ui.utils import getFont
+from ui.components.backbutton.back_button import *
 from PyQt5 import QtCore, QtGui, QtWidgets, QtSvg
 
 class ArticleDetail(QtWidgets.QWidget):
@@ -27,6 +28,9 @@ class ArticleDetail(QtWidgets.QWidget):
         contentWidget = QtWidgets.QWidget(scrollArea)
         contentWidget.setMinimumWidth(scrollArea.width())
         scrollArea.setWidget(contentWidget)
+
+        # BACK
+        back_button = BackButton(parent)
 
         # TITLE
         article_title_container = QtWidgets.QHBoxLayout()
@@ -78,24 +82,24 @@ class ArticleDetail(QtWidgets.QWidget):
         pixmap = QtGui.QPixmap("assets/images/" + article.image_path)
         scaled_pixmap = pixmap.scaled(500, 375, QtCore.Qt.KeepAspectRatio)
         article_image.setPixmap(scaled_pixmap)
-        article_image.setFixedHeight(375)
-        article_image.setFixedWidth(500)
         article_image.setObjectName("article_image")
-        article_image.setStyleSheet("border-radius: 8px; border-style: solid; border-color: black; border-width: 1px;")
         article_image.setAlignment(QtCore.Qt.AlignCenter)
 
         # CONTENT
         article_content = QtWidgets.QLabel()
         article_content.setMaximumWidth(int(0.8*parentWidth))
-        article_content.setText(article.content)
+        formatted_content = article.content.replace('\n', '\n\n         ')
+        article_content.setText(formatted_content)
         article_content.setObjectName("article_content")
         article_content.setFont(getFont("Regular", 12))
         article_content.setWordWrap(True)
+        article_content.setContentsMargins(0, 0, 0, 24)
         article_content.setAlignment(QtCore.Qt.AlignJustify)
 
         # LAYOUT
         self.layout = QtWidgets.QVBoxLayout(contentWidget)
         self.layout.setContentsMargins(0,0,0,0)
+        self.layout.addWidget(back_button, alignment=QtCore.Qt.AlignmentFlag.AlignLeft)
         self.layout.addLayout(article_title_container)
         self.layout.addLayout(article_detail_container)
         self.layout.addWidget(article_image, alignment=QtCore.Qt.AlignmentFlag.AlignHCenter)
