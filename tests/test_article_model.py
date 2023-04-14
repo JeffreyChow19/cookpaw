1a4c373941f13f7ac0eb26d3570a50ed81026e0e
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-04-15 01:50:01 +0700

    fix: create function test_article_model

--- tests/test_article_model.py
+++ tests/test_article_model.py
@@ -6,17 +6,18 @@ sys.path.append(root_dir)
 
 from src.models.article import *
 
-title = "Article Title"
-content = "Article Content"
-author = "Article Author"
-publish_date = "Article Publish Date"
-path = "Article Image Path"
+def test_article_model():
+    title = "Article Title"
+    content = "Article Content"
+    author = "Article Author"
+    publish_date = "Article Publish Date"
+    path = "Article Image Path"
 
-a = Article(0, title, content, author, publish_date, path)
+    a = Article(0, title, content, author, publish_date, path)
 
-assert a.article_id == 0
-assert a.title == title
-assert a.content == content
-assert a.publish_date == publish_date
-assert a.author == author
-assert a.image_path == path
+    assert a.article_id == 0
+    assert a.title == title
+    assert a.content == content
+    assert a.publish_date == publish_date
+    assert a.author == author
+    assert a.image_path == path
