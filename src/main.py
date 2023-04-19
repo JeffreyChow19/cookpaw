24c4df05b9926b8e203b041979cdebf0ba69264f
yuujin-Q <13521074@std.stei.itb.ac.id>
2023-04-19 18:03:25 +0700

    refactor: icon filename

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ class MainWindow(QMainWindow):
         
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self, self.articles, self.recipes)
-        self.setWindowIcon(QtGui.QIcon("assets/icons/Asset 2.ico"))
+        self.setWindowIcon(QtGui.QIcon("assets/icons/cookpaw.ico"))
         self.setWindowTitle("Cookpaw")
     
 
