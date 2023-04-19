7e8f7b9df20ae6c2bbd45511d6e705d2f868d5d1
yuujin-Q <13521074@std.stei.itb.ac.id>
2023-04-19 11:06:40 +0700

    feat: srgb convertion in utils

--- src/ui/utils.py
+++ src/ui/utils.py
@@ -1,8 +1,14 @@
 from PyQt5 import QtGui
+import io
+import os
+import shutil
+from PIL import Image, ImageCms
 
-def getFont(type, size):
+
+def getFont(font_weight, font_size):
+    """Get font from assets"""
     # Set the path to the font file based on the type
-    font_path = f"assets/fonts/poppins/Poppins-{type}.ttf"
+    font_path = f"assets/fonts/poppins/Poppins-{font_weight}.ttf"
 
     # Add the font to the application
     font_id = QtGui.QFontDatabase.addApplicationFont(font_path)
@@ -14,11 +20,39 @@ def getFont(type, size):
     font = QtGui.QFont(font_family)
 
     # Set the font size
-    font.setPointSize(size)
-    if (type == "Bold"):
+    font.setPointSize(font_size)
+    if (font_weight == "Bold"):
         font.setWeight(75)
 
     return font
 
-# def getColor(color):
-#     return 
+
+def convert_icc_to_icc_srgb(image_path):
+    """Convert PIL image to sRGB color space (if possible and if image is in other ICC profiles)"""
+    img = Image.open(image_path)
+    icc = img.info.get('icc_profile', '')
+    if icc:
+        io_handle = io.BytesIO(icc)     # virtual file
+        src_profile = ImageCms.ImageCmsProfile(io_handle)
+        dst_profile = ImageCms.createProfile('sRGB')
+        img_conv = ImageCms.profileToProfile(img, src_profile, dst_profile)
+        icc_conv = img_conv.info.get('icc_profile','')
+        if icc != icc_conv:
+            # ICC profile was changed, converted file is saved
+            img_conv.save(image_path,
+                        format = 'JPEG',
+                        quality = 50,
+                        icc_profile = icc_conv)
+
+
+def save_image_to_assets(source_image_path):
+    """Copy and save an external image to assets folder"""
+    # copy source image to assets
+    destination_path = 'assets/images/images_recipe/' + os.path.basename(source_image_path)
+    print(destination_path)
+    shutil.copy(source_image_path, destination_path)
+
+    # handle complex icc profiles in assets folder
+    convert_icc_to_icc_srgb(destination_path)
+
+    return destination_path
